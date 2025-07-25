305afc36bcdca1d54eabe3403d31e8380df19725
ryeorin <flsk2815@gmail.com>
2025-07-25 13:36:06 +0900

    feat #18 - 지도에 매물 위치 띄우기

---
+++ backend/src/main/java/org/scoula/map/dto/ListingMapDTO.java
@@ -0,0 +1,13 @@
+package org.scoula.map.dto;
+
+public class ListingMapDTO {
+
+    //1. 매물 고유 ID
+    private Long id;
+
+    //2. 위도
+    private Double latitude;
+
+    //3. 경도
+    private Double longitude;
+}
