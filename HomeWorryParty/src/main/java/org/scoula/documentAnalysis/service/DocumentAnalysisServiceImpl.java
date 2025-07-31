6ac93ba46adb89c1e826a91ff155e83a944f0c6a
YuBin Kim <yubin412@naver.com>
2025-07-31 09:26:39 +0900

    chore #37 - 서류 분석 결과 내용 수정

--- HomeWorryParty/src/main/java/org/scoula/documentAnalysis/service/DocumentAnalysisServiceImpl.java
+++ HomeWorryParty/src/main/java/org/scoula/documentAnalysis/service/DocumentAnalysisServiceImpl.java
@@ -49,7 +49,7 @@ public class DocumentAnalysisServiceImpl implements DocumentAnalysisService {
             documentAnalysisResultDTO.getDescriptionTitleList().add("확인되지 않은 중개인");
             documentAnalysisResultDTO.getDescriptionContentList()
                     .add("거래 기록이나 정보가 존재하지 않은 중개인입니다. 거래시 추가적인 확인이나" +
-                            "주의가 필요합니다." + "<br></br>");
+                            "주의가 필요합니다.");
             documentAnalysisResultDTO.setScore(documentAnalysisResultDTO.getScore() - (15));
         }
     }
@@ -63,16 +63,14 @@ public class DocumentAnalysisServiceImpl implements DocumentAnalysisService {
             documentAnalysisResultDTO.getDescriptionTitleList().add("등기부등본 서류 확인 미흡");
             documentAnalysisResultDTO.getDescriptionContentList().add(
                     "1. 명의 도용 및 권리관계 불일치<br></br>" +
-                            "   실제 소유자가 아닌 제3자가 임대인(매도인) 행세를 하여 무효 계약이나 사기가 발생할 수 있음>" +
+                            "   실제 소유자가 아닌 제3자가 임대인(매도인) 행세를 하여 무효 계약이나 사기가 발생할 수 있음" +
                             "<br></br>" +
                             "   공동소유, 상속 등 권리관계가 복잡한 경우 실제 계약 당사자가 아닌 사람이 거래에 나설 가능성" +
                             "<br></br>" +
                             "2. 근저당·가압류 등 숨겨진 권리 부담<br></br>" +
-                            "   등기부에 근저당권, 가압류, 압류, 전세권 등 기타 권리가 설정되어 \n" +
+                            "   등기부에 근저당권, 가압류, 압류, 전세권 등 기타 권리가 설정되어, 보증금 반환이나 소유권 이전에 심각한 차질 발생" +
                             "<br></br>" +
-                            "   보증금 반환이나 소유권 이전에 심각한 차질 발생" +
-                            "<br></br>" +
-                            "   잔금지급 후 부동산이 경매로 넘어가는 경우, 전세금·보증금 손실" +
+                            "   잔금 지급 후 부동산이 경매로 넘어가는 경우, 전세금·보증금 손실" +
                             "<br></br>" +
                             "3. 이중매매·이중계약 가능성<br></br>" +
                             "   이미 매매(임대)가 진행 중이거나 타인에게 담보로 잡혀 있는데도 모른 채 거래" +
@@ -80,9 +78,7 @@ public class DocumentAnalysisServiceImpl implements DocumentAnalysisService {
                             "   같은 부동산을 여러 명에게 중복 계약할 수 있음" +
                             "<br></br>" +
                             "4. 허위매물 및 무권리자와의 거래<br></br>" +
-                            "   실제 등기상 소유자와 계약 상대방의 일치 여부를 확인 못해" +
-                            "<br></br>" +
-                            "   존재하지 않는 매물(허위매물)로 인한 계약금·보증금 사기" +
+                            "   실제 등기상 소유자와 계약 상대방의 일치 여부를 확인 못해, 존재하지 않는 매물(허위매물)로 인한 계약금·보증금 사기" +
                             "<br></br>" +
                             "5. 재산상 손해 및 법적 분쟁<br></br>" +
                             "   계약이 무효화되어도 이미 지급한 계약금, 중도금, 보증금을 회수하지 못하는 위험" +
@@ -147,14 +143,14 @@ public class DocumentAnalysisServiceImpl implements DocumentAnalysisService {
         if(percent < 0) {
             documentAnalysisResultDTO.getDescriptionTitleList().add("데이터가 없는 유형의 매물");
             documentAnalysisResultDTO.getDescriptionContentList()
-                    .add("해당 지역, 평수에 맞는 다른 매물이 탐색되지 않기 때문에 주의가 필요합니다." + "<br></br>");
+                    .add("해당 지역, 평수에 맞는 다른 매물이 탐색되지 않기 때문에 주의가 필요합니다.");
         }
 
         if (percent > 5) {
             documentAnalysisResultDTO.getDescriptionTitleList().add("시세보다 싼 가격");
             documentAnalysisResultDTO.getDescriptionContentList()
                     .add("시세보다 " + percent + "% 저렴하기에 거래시 불합리한 조건, 깡통 전세, 보증금 사기등의 " +
-                            "문제가 발생할 수 있기에 거래시 주의가 필요합니다." + "<br></br>");
+                            "문제가 발생할 수 있기에 거래시 주의가 필요합니다.");
             documentAnalysisResultDTO.setScore(documentAnalysisResultDTO.getScore() - (percent * 2));
         }
     }
